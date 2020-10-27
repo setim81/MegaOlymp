9408947d0c3fe59f2fcfd80bc7087ee335bbc63c
setim81 <54441661+setim81@users.noreply.github.com>
2020-10-27 12:49:50 +0300

    copy

---
+++ js/scroll_top.js
@@ -0,0 +1,25 @@
+//скрипт кнопки вверх 
+
+	
+	$(document).ready(function(){
+	  $('body').append('<a href="#" id="go-top" title="Вверх"> 	&perp;</a>');
+	});
+
+	$(function() {
+	 $.fn.scrollToTop = function() {
+	  $(this).hide().removeAttr("href");
+	  if ($(window).scrollTop() >= "250") $(this).fadeIn("slow")
+	  var scrollDiv = $(this);
+	  $(window).scroll(function() {
+	   if ($(window).scrollTop() <= "250") $(scrollDiv).fadeOut("slow")
+	   else $(scrollDiv).fadeIn("slow")
+	  });
+	  $(this).click(function() {
+	   $("html, body").animate({scrollTop: 0}, "slow")
+	  })
+	 }
+	});
+
+	$(function() {
+	 $("#go-top").scrollToTop();
+	});
